9cd053c5ed8e642b15dd2153afaf3ac1e026c92d
Luis Lau <theauthorgm@gmail.com>
2019-08-04 21:38:14 -0500

    Add boilerplate for Entity interface

--- src/core.ts
+++ src/core.ts
@@ -1,9 +1,11 @@
 import {KoratServer, createServer} from './server';
 import {KoratPluginManager, createPluginManager} from './plugins';
 import {KoratConfig, createConfig} from './config';
+import {KoratEntityManager, createEntityManager} from './entities';
 
 export class KoratCore {
   server: KoratServer = createServer(this);
   config: KoratConfig = createConfig(this);
+  entities: KoratEntityManager = createEntityManager(this);
   plugins: KoratPluginManager = createPluginManager(this);
 }
---
+++ src/entities.ts
@@ -0,0 +1,19 @@
+import {KoratCore} from './core';
+
+export interface KoratEntityType {
+
+}
+
+export interface KoratEntity {
+
+}
+
+export interface KoratEntityManager {
+
+}
+
+export function createEntityManager(core: KoratCore): KoratEntityManager {
+  return {
+
+  };
+}
